2fd7c48610034474ffab4dd8fea92ac90ecd898e
fourat153 <85787866+fourat153@users.noreply.github.com>
2023-11-21 19:48:37 +0100

    Delete mini_project directory

--- mini_project/x1.X/build/default/debug/module/uartlogging/uartloggging.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/debug/module/uartlogging/uartloggging.p1:  \
-module/uartlogging/uartloggging.c  \
-module/uartlogging/uartlogging.h  \
-module/uartlogging/../../mcc_generated_files/eusart.h 
--- mini_project/x1.X/module/uartlogging/uartloggging.c
+++
@@ -1,9 +0,0 @@
-#include "uartlogging.h"
-
-void uart_loggingDebugData(uint8_t * str , uint16_t str_length){
-    for ( uint8_t i  = 0 ; i < str_length ; i++ )
-    {
-        EUSART_Write(*str++);
-    }
-}
-
