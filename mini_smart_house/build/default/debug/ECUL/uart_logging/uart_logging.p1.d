056179738c9997da7b461dfaad5fbab1afe67bdb
fourat153 <85787866+fourat153@users.noreply.github.com>
2024-01-30 09:53:52 +0100

    Delete mini_smart_house directory

--- mini_smart_house/build/default/debug/ECUL/uart_logging/uart_logging.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/debug/ECUL/uart_logging/uart_logging.p1:  \
-ECUL/uart_logging/uart_logging.c  \
-ECUL/uart_logging/../uart_logging/uart_logging.h  \
-ECUL/uart_logging/../../mcc_generated_files/eusart.h 
--- mini_smart_house/real_time_module/real_time_clock.c
+++
@@ -1,8 +0,0 @@
-#include "../real_time_module/real_time.h"
-#include "real_time.h"
-static real_time_clock_t real_time; 
-real_time_clock_t get_Real_time_clock (void) 
-{
-    real_time.seconds = I2C_Read1ByteRegister( 0x68, 0x0);  // D0 == 11010000  last bit is for read or write  send 0xD0 / 2 ITS A BUG  
-    return real_time ;
-}
